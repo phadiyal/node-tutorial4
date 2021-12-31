aa2d95734328318b938888141c78c52bcbe2751d
phadiyal <phadiyal620@rku.ac.in>
2021-12-31 09:21:44 +0530

    secoung completed

---
+++ secound.js
@@ -0,0 +1,12 @@
+var sum=(a,b)=>{
+    return  a+b;
+}
+var mul=(a,b)=>{
+    return a*b;
+}
+var div=(a,b)=>{
+    return a/b;
+}
+module.exports.add= sum;
+module.exports.multi= mul;
+module.exports.divi= div;
---
+++ third.js
@@ -0,0 +1,4 @@
+var text = require('./secound');
+console.log(text.add(10,20));
+console.log(text.multi(10,20));
+console.log(text.divi(10,20));
